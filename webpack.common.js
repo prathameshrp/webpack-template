b98b06858f5dda4217d28b93ef5520d1c9fdc9af
prathamesh <prathamesh.math@gmail.com>
2024-08-19 23:51:05 +0530

    Initial commit setup

---
+++ webpack.common.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "main.bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+  
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+            // favicon: "./src/assets/images/fevicon.ico",
+        }),
+    ],
+    module: {
+        rules:[
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            }
+            ,
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+                type: "asset/resource",
+            },
+            
+        ],
+    },
+}
